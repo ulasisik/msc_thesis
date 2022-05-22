8239539fd560b229989e334b3daeb299a1f7b9cd
ulas isildak <isildak.ulas@gmail.com>
2022-05-22 17:01:06 +0200

    Add and update scripts used to plot method figures

--- scripts/get_data_kang.R
+++ scripts/get_data_kang.R
@@ -2,40 +2,45 @@ library(preprocessCore)
 library(tidyverse)
 library(GEOquery)
 library(biomaRt)
+library(oligo)
 
 # # Get data from geo
-# gse <- getGEO("GSE25219", GSEMatrix = TRUE)[[1]]
+gse <- getGEO("GSE25219", GSEMatrix = TRUE)
 
-kang = getGEOSuppFiles("GSE25219", baseDir="/Users/ulas/Projects/msc_thesis/data/raw", makeDirectory = FALSE)
+# kang = getGEOSuppFiles("GSE25219", baseDir="/Users/ulas/Projects/msc_thesis/data/raw", makeDirectory = FALSE)
 
-cel_cbc_files = list.files("/Users/ulas/Projects/msc_thesis/data/raw/GSE25219", full.names=TRUE, pattern="CEL")
+cel_files = list.files("/Users/ulas/Projects/msc_thesis/data/raw/GSE25219_RAW", full.names=TRUE, pattern="CEL")
+data = read.celfiles(cel_files)
 
-# Save meta data
-mdt = pData(gse)
-readr::write_rds(mdt, "/Users/ulas/Projects/msc_thesis/data/kang_metadata.rds")
+ppdata = rma(data, background=TRUE, normalize=FALSE, target="core")
 
-# Exctract ages
-agemap = mdt %>%
-  dplyr::select("geo_accession", "age:ch1") %>%
-  dplyr::rename(age = "age:ch1") %>%
-  filter(age>0)
+exprdata = exprs(ppdata)
+# # Save meta data
+# mdt = pData(data)
+# readr::write_rds(mdt, "/Users/ulas/Projects/msc_thesis/data/kang_metadata.rds")
+# 
+# # Exctract ages
+# agemap = mdt %>%
+#   dplyr::select("geo_accession", "age:ch1") %>%
+#   dplyr::rename(age = "age:ch1") %>%
+#   filter(age>0)
 
-# Get ensembl gene IDs
-gmap = fData(gse) %>%
-  dplyr::select(ID, Entrez_Gene_ID, Gene_Symbol)
-
-ensmbl = getBM(attributes = c('hgnc_symbol', 'ensembl_gene_id'),
-               filters = 'hgnc_symbol', 
-               values = gmap$Gene_Symbol, 
-               mart = useMart("ensembl",dataset="hsapiens_gene_ensembl")) %>%
-  rename(Gene_Symbol = hgnc_symbol, Ensembl_ID = ensembl_gene_id)
-
-genemap = gmap %>%
-  left_join(ensmbl, by="Gene_Symbol") %>%
-  drop_na()
+# # Get ensembl gene IDs
+# gmap = fData(data) %>%
+#   dplyr::select(ID, Entrez_Gene_ID, Gene_Symbol)
+# 
+# ensmbl = getBM(attributes = c('hgnc_symbol', 'ensembl_gene_id'),
+#                filters = 'hgnc_symbol', 
+#                values = gmap$Gene_Symbol, 
+#                mart = useMart("ensembl",dataset="hsapiens_gene_ensembl")) %>%
+#   rename(Gene_Symbol = hgnc_symbol, Ensembl_ID = ensembl_gene_id)
+# 
+# genemap = gmap %>%
+#   left_join(ensmbl, by="Gene_Symbol") %>%
+#   drop_na()
 
 # Get expression data
-expr = exprs(gse) %>%
+expr = exprs(data) %>%
   as.data.frame() %>%
   rownames_to_column(var="gnames") %>%
   dplyr::select(gnames, agemap$geo_accession) %>% 
--- scripts/get_data_somel.R
+++ scripts/get_data_somel.R
@@ -5,23 +5,27 @@ library(biomaRt)
 library(oligo)
 
 ## Get data from geo
+gse_cbc =  getGEO("GSE22569")[[1]]
+
+cbc_sample = pData(gse_cbc)
+cbc_feature = fData(gse_cbc)
 # gse_cbc <- getGEOSuppFiles("GSE22569", baseDir = "/Users/ulas/Projects/msc_thesis/data/raw", makeDirectory = F)
 # gse_pfc <- getGEOSuppFiles("GSE18069", baseDir = "/Users/ulas/Projects/msc_thesis/data/raw", makeDirectory = F)
 
 ## Read CEL files
-cel_cbc_files = list.files("/Users/ulas/Projects/msc_thesis/data/raw/GSE22569", full.names=TRUE, pattern="CEL")
-cel_pfc_files = list.files("/Users/ulas/Projects/msc_thesis/data/raw/GSE18069", full.names=TRUE, pattern="CEL")
+cel_cbc_files = list.files("/Users/ulas/Projects/msc_thesis/data/raw/GSE22569_RAW", full.names=TRUE, pattern="CEL")
+# cel_pfc_files = list.files("/Users/ulas/Projects/msc_thesis/data/raw/GSE18069_RAW", full.names=TRUE, pattern="CEL")
 
 data_cbc = read.celfiles(cel_cbc_files)
-data_pfc = read.celfiles(cel_pfc_files)
+# data_pfc = read.celfiles(cel_pfc_files)
 
 ## RMA Correction
 ppdata_cbc = rma(data_cbc, background=TRUE, normalize=FALSE, target="core")
-ppdata_pfc = rma(data_pfc, background=TRUE, normalize=FALSE, target="core")
+# ppdata_pfc = rma(data_pfc, background=TRUE, normalize=FALSE, target="core")
 
 ## Get expressions
 expr_cbc = exprs(ppdata_cbc)
-expr_pfc = exprs(ppdata_pfc)
+# expr_pfc = exprs(ppdata_pfc)
 
 oligo::boxplot(expr_cbc)
 oligo::hist(expr_cbc)
